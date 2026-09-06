8ab95f99f2ab32ba57c0080d45fd414a0f93794a
dukefong8 <dukefong8@gmail.com>
2026-09-06 12:07:55 -0400

    update

--- hs/src/Htmx.hs
+++ hs/src/Htmx.hs
@@ -15,7 +15,9 @@ pageShell customHead body = [hsx|
   <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <script defer src="https://cdn.jsdelivr.net/npm/htmx.org@next"></script>
+      <script defer src="https://cdn.jsdelivr.net/npm/htmx.org@next/dist/htmax.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/scittle@0.8.32/dist/scittle.js" type="application/javascript"></script>
+      <script src="https://raw.githubusercontent.com/borkdude/reagami/refs/heads/main/src/reagami/core.cljc" type="application/x-scittle"></script>
       {customHead}
     </head>
     <body>
